eaca25cbd510995fa2c2c47448be5cf71a389502
RT Nalan Sarrwin <gopikrish.thiru@gmail.com>
2022-06-23 22:40:01 +0530

    Navigation components and adding the communication between the fragments

--- app/src/main/java/com/amalwin/newsapiclient/MainActivity.kt
+++ app/src/main/java/com/amalwin/newsapiclient/MainActivity.kt
@@ -2,10 +2,20 @@ package com.amalwin.newsapiclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.amalwin.newsapiclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)!!
+            .findNavController()
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
